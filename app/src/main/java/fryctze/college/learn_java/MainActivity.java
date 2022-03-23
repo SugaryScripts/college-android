0b42b016a03cf0df8362f9d2e697848059c8a2e3
Fikry Farenza <vickyfarenza@gmail.com>
2022-03-24 06:58:14 +0700

    Static & dynamic list :sparkles:

--- app/src/main/java/fryctze/college/learn_java/MainActivity.java
+++ app/src/main/java/fryctze/college/learn_java/MainActivity.java
@@ -3,6 +3,9 @@ package fryctze.college.learn_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 import fryctze.college.learn_java.databinding.ActivityMainBinding;
 
@@ -15,5 +18,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        loadList();
+    }
+
+    /**
+     * attribute entries in main activity layout, got replaced in here
+     * and update automatically when list on change
+     */
+    private void loadList() {
+        ArrayList<String> items = new ArrayList<>();
+        items.add("Kubuntu");
+        items.add("Ubuntu"); // removed
+        items.add("Kali");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                items
+        );
+
+        binding.lvOpSys.setAdapter(adapter);
+
+        // updated/added new item automatically without re-definition adapter
+        items.add("Tiny core");
+        items.remove("Ubuntu");
     }
 }
