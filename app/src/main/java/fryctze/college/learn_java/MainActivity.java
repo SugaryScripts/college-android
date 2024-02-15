a72a2d09335deeb6f96b19f5d6fc2ff29fd61cc6
Fikry C Farenza <vickyfarenza@gmail.com>
2024-02-16 05:04:30 +0700

    WIP on learn-listview

--- app/src/main/java/fryctze/college/learn_java/MainActivity.java
+++ app/src/main/java/fryctze/college/learn_java/MainActivity.java
@@ -6,10 +6,18 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 import fryctze.college.learn_java.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String[] WORDS = {
+            "contract", "a binding enforced by law",
+            "assurance", "agreement to do or give ..",
+            "engage", "consume on one's attention or time"
+    };
+    private HashMap<String, String> dictionary;
 
     private ActivityMainBinding binding;
 
@@ -19,8 +27,38 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        loadList();
+        letsPlay();
     }
+
+    private void letsPlay() {
+        ArrayList<String> word = new ArrayList<>();
+        dictionary = new HashMap<>();
+        for (int i = 0; i < WORDS.length; i+=2) {
+            word.add(WORDS[i]);
+            dictionary.put(WORDS[i], WORDS[i+1]);
+        }
+
+        Collections.shuffle(word);
+        String quiz = word.get(0);
+        binding.tvQuiz.setText(quiz);
+
+        ArrayList<String> Options = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Options.add(dictionary.get(word.get(i)));
+        }
+
+        Collections.shuffle(Options);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Options
+        );
+
+        binding.lvOpSys.setAdapter(arrayAdapter);
+
+    }
+
 
     /**
      * attribute entries in main activity layout, got replaced in here
